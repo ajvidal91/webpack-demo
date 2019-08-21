0e36cb2ca08e1f3e2fb3f61aba023d79c4e02fec
angvidal <angvidal@publicisgroupe.net>
2019-08-21 10:53:02 -0400

    configured the assets portion of the webpack documentation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './my-image.jpg';
+import Data from './data.xml';
 
 function component() {
-  const element = document.createElement('div');
+    const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
 
-  return element;
+    element.appendChild(myIcon);
+
+    console.log(Data);
+
+    return element;
 }
 
 document.body.appendChild(component());
