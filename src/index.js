a25f4ac393945ebfd0c12ef34162a319928588fa
angvidal <angvidal@publicisgroupe.net>
2019-08-21 14:52:10 -0400

    installed sass loader and glide.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
-import './styles.css';
+import './main.scss';
 
 function component() {
     const element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,8 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            test: /\.scss$/,
+            use: ['style-loader', 'css-loader', 'sass-loader']
         }]
     },
     plugins: [
